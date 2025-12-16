93de8a2a8b5154f7f24b0ab5cd00cb19f16a027d
Jeet Mondal <145958423+FantamKing@users.noreply.github.com>
2025-12-16 22:18:25 +0530

    Delete js directory

--- js/home.js
+++
@@ -1,8 +0,0 @@
-import { loadPage } from './main.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Shop Now button
-    document.getElementById('shop-now-btn')?.addEventListener('click', () => {
-        loadPage('categories');
-    });
-});
